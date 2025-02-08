f60e5d4e7fb1340697c469bdfa1f483251ec5e06
cariocaphil <cariocaphil@gmail.com>
2025-02-08 18:48:06 +0100

    adjust to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import HomePage from "./pages/HomePage";
 
